019d0077e220a1806dba034fdc4a65b2fcbee6b5
Jonathan Haigh <jonathanhaigh@gmail.com>
2026-06-23 13:21:49 +0100

    Add tests for using imports at module scope

--- tests/test_import.py
+++ tests/test_import.py
@@ -53,11 +53,40 @@ def test_import_var(tmp_path):
     check_prog_output(tmp_path, main_src, "", 11, a=a_src)
 
 
+def test_import_var_use_comptime(tmp_path):
+    main_src = """
+    import a;
+    let y = a::x;
+    pub fn main() i32 {
+        return y;
+    }
+    """
+    a_src = """
+    pub let x = 11;
+    """
+    check_prog_output(tmp_path, main_src, "", 11, a=a_src)
+
+
 def test_import_private_var(tmp_path):
     main_src = """
     import a;
     pub fn main() i32 {
         return a::x;
+    }
+    """
+    a_src = """
+    let x = 11;
+    """
+    with pytest.raises(ItemNotFoundError):
+        compile_modules(tmp_path, main=main_src, a=a_src)
+
+
+def test_import_private_var_use_comptime(tmp_path):
+    main_src = """
+    import a;
+    let y = a::x;
+    pub fn main() i32 {
+        return y;
     }
     """
     a_src = """
@@ -83,11 +112,44 @@ def test_import_typ(tmp_path):
     check_prog_output(tmp_path, main_src, "", 32, a=a_src)
 
 
+def test_import_typ_use_comptime(tmp_path):
+    main_src = """
+    import a;
+    let x = a::T{int: 32};
+    pub fn main() i32 {
+        return x.int;
+    }
+    """
+    a_src = """
+    pub struct T {
+        int: i32,
+    }
+    """
+    check_prog_output(tmp_path, main_src, "", 32, a=a_src)
+
+
 def test_import_private_typ(tmp_path):
     main_src = """
     import a;
     pub fn main() i32 {
         let x = a::T{int: 32};
+        return x.int;
+    }
+    """
+    a_src = """
+    struct T {
+        int: i32,
+    }
+    """
+    with pytest.raises(ItemNotFoundError):
+        compile_modules(tmp_path, main=main_src, a=a_src)
+
+
+def test_import_private_typ_use_comptime(tmp_path):
+    main_src = """
+    import a;
+    let x = a::T{int: 32};
+    pub fn main() i32 {
         return x.int;
     }
     """
